75c2e33c7dc464cef23f7aa88b3926d723b768b6
itzmarwin <itzm4rw1n@gmail.com>
2025-03-23 18:26:44 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -4,22 +4,18 @@ import pymongo
 from config import DB_URI, DB_NAME
 
 dbclient = pymongo.MongoClient(DB_URI)
-database = dbclient[DB_NAME]  # "Anon" database connect hoga
-
-# Old Mikasa bot ke users ko fetch karne ke liye collection name 'tgusersdb' set kiya
-user_data = database['tgusersdb']  
+database = dbclient[DB_NAME]
+user_data = database['tgusersdb']
 
 async def present_user(user_id: int):
-    found = user_data.find_one({'_id': user_id})
-    return bool(found)
+    return bool(user_data.find_one({'_id': user_id}))
 
 async def add_user(user_id: int):
     user_data.insert_one({'_id': user_id})
-    return
 
 async def full_userbase():
-    return [doc['_id'] async for doc in user_data.find()]
+    # Async for की जगह synchronous loop use करें
+    return [doc['_id'] for doc in user_data.find({}, {'_id': 1})]
 
 async def del_user(user_id: int):
     user_data.delete_one({'_id': user_id})
-    return
