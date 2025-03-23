5634be6b2885d0d94df75851a1908f3603e9aa2e
itzmarwin <itzm4rw1n@gmail.com>
2025-03-24 00:26:56 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,21 +1,27 @@
 #(©)CodeXBotz
 
-import pymongo
+from motor.motor_asyncio import AsyncIOMotorClient
 from config import DB_URI, DB_NAME
 
-dbclient = pymongo.MongoClient(DB_URI)
-database = dbclient[DB_NAME]
-user_data = database['tgusersdb']
+client = AsyncIOMotorClient(DB_URI)
+database = client[DB_NAME]
+
+# Use same collection name as music bot
+user_data = database.tgusersdb  # Changed from 'users' to 'tgusersdb'
 
 async def present_user(user_id: int):
-    return bool(user_data.find_one({'_id': user_id}))
+    user = await user_data.find_one({"user_id": user_id})  # Changed _id to user_id
+    return bool(user)
 
 async def add_user(user_id: int):
-    user_data.insert_one({'_id': user_id})
+    await user_data.update_one(
+        {"user_id": user_id},
+        {"$set": {"user_id": user_id}},
+        upsert=True
+    )
 
 async def full_userbase():
-    # Async for की जगह synchronous loop use करें
-    return [doc['_id'] for doc in user_data.find({}, {'_id': 1})]
+    return [doc["user_id"] async for doc in user_data.find({"user_id": {"$gt": 0}}, {"user_id": 1})]  # Music bot style query
 
 async def del_user(user_id: int):
-    user_data.delete_one({'_id': user_id})
+    await user_data.delete_one({"user_id": user_id})
